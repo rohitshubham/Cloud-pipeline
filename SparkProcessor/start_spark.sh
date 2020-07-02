6f40876e8ce214d0bbbcdfa6a19be7ea18786c67
Rohit Raj <rohit.raj@aalto.fi>
2020-07-02 09:54:52 +0000

    Updated the way dockerfile for spark-processor is created

--- SparkProcessor/start_spark.sh
+++ SparkProcessor/start_spark.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-export SPARK_HOME=/opt/spark
-export PATH=$PATH:/opt/spark/bin:/opt/spark/sbin
-export PYSPARK_PYTHON=python3.6
 
-spark-submit --master "spark://spark:7077" --packages  org.apache.spark:spark-streaming-kafka-0-8-assembly_2.11:2.4.6 pyspark_processor.py
+spark-submit --master "$MASTER"  --packages $PACKAGES  $1
